319854628c4bd4188adc7ef2c7633a1ef96f5d4f
Frankmer <frankmer@outlook.com>
2025-11-11 14:59:54 -0400

    Added comment and improve the core

--- Test/cypress/e2e/QAPractice.cy.js
+++ Test/cypress/e2e/QAPractice.cy.js
@@ -1,12 +1,35 @@
+var movieTitle = "Inception d";
+var backendUrl = 'http://localhost:3000';
+
 describe('QA Practice', () => {
-  it('Search Movie Error', () => {
-    cy.visit('/')
-    cy.get('input[placeholder="Search movies..."]').type('Fast and Furious')
-    cy.get('button[type="submit"]').click()
-    cy.get("p").contains("Search failed").should("be.visible")
+  before(() => {
+    // Runs once before all tests in the block
+    cy.request({
+        method: 'POST', //Method POST to add a new movie
+        url: backendUrl+"/favorites", //Favorites endpoint
+        headers: {
+          'OMDB_API_KEY': 'demo'    //Header with API key
+        },
+        body: { //Request body with movie details
+            "imdbID": "tt1375667",
+            "Title": movieTitle,
+            "Year": "2010",
+            "Poster": "testimage.jpg"
+        }
+    })
   })
+  
+  it('Search Movie', () => {
+    cy.visit('/') //Visit the base URL
+    cy.get('input[placeholder="Search movies..."]').type(movieTitle) //Type movie title in search input
+    cy.get('button[type="submit"]').click() //Click the search button
+    cy.get("h4").contains(movieTitle).should("be.visible") //Verify that the movie appears in the results
+  })
+
   it('Remove from Favorites', () => {
     cy.visit('/')
-    cy.get("h4").contains("Fast and Furios").parent().find("button").click()
+    //I did create a test movie with this title to test the remove from favorites functionality
+    cy.get("h4").contains(movieTitle).parent().find("button").click() 
+    cy.get("h4").contains(movieTitle).should("not.exist") //Verify that the movie has been removed from favorites
   })  
 })
