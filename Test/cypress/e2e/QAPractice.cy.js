fb92f0e23a5ce7fddd2df03d95c64b0c7c35c33c
Frankmer <frankmer@outlook.com>
2025-11-12 19:42:48 -0400

    Remove after adding test case added.

--- Test/cypress/e2e/QAPractice.cy.js
+++ Test/cypress/e2e/QAPractice.cy.js
@@ -12,7 +12,7 @@ describe('QA Practice', () => {
     cy.visit('/') //Visit the base URL before each test
   });
 
-  it.skip('Search Movies', () => {
+  it('Search Movies', () => {
     cy.log("Movie selected: "+testMovies[0]);
     cy.get('input[placeholder="Search movies..."]').type(testMovies[0]) //Type movie title in search input
     cy.get('button[type="submit"]').click() //Click the search button
@@ -40,5 +40,20 @@ describe('QA Practice', () => {
       cy.get("h4").contains(movie).should("not.exist") //Verify that the movie has been removed from favorites
       cy.screenshot('Evidences/Remove_from_Favorites/'+movie+"_test") //Take screenshot of the favorite movie removed
     })  
-})
+  })
+
+  it('Remove movie right after adding it to favorites via search results', () => {
+    const movie = testMovies[0];
+    cy.log("Movie selected: "+movie); //Log the movie being processed
+    cy.get('input[placeholder="Search movies..."]').clear().type(movie) //Type movie title in search input
+    cy.get('button[type="submit"]').click() //Click the search button
+    cy.get("h3").contains(movie).should("be.visible") //Verify that the movie appears in the results
+    cy.get("h3").contains(movie).parent().find("button").click() //Click the Add to Favorites button for the movie
+    cy.get("h4").contains(movie).should("be.visible") //Verify that the movie has been added to favorites
+    cy.screenshot('Evidences/Remove_right_after_adding/Added_'+movie+"_beforeRemove_test") //Take screenshot of the favorite movie added
+    cy.get("button").contains("Remove from Favorites").click() //Click the Remove from 
+    cy.screenshot('Evidences/Remove_right_after_adding/Added_'+movie+"_afterRemove_test") //Take screenshot of the favorite movie removed
+    cy.get("h4").contains(movie).should("not.exist") //Verify that the movie has been removed from favorites
+  });
+
 })
