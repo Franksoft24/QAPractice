1179894b17889f7a6a9f2c0d24f74612f41e538b
Frankmer <frankmer@outlook.com>
2025-11-12 18:08:14 -0400

    Improve the code

--- Test/cypress/e2e/EndpointTestSuite.cy.js
+++ Test/cypress/e2e/EndpointTestSuite.cy.js
@@ -22,9 +22,6 @@ describe("Endpoint Test Suite", () => {
     cy.request({
         method: 'POST', //Method POST to add a new movie
         url: backendUrl+"/favorites", //Favorites endpoint
-        headers: {
-          'OMDB_API_KEY': 'demo'    //Header with API key
-        },
         body: testData //Request body with movie details from fixture
     }).then((response) => {
         expect(response.status).to.eq(200); //Expecting status code 200 for successful addition.
@@ -35,9 +32,6 @@ describe("Endpoint Test Suite", () => {
     cy.request({
         method: 'GET', //Method GET to retrieve favorite movies
         url: backendUrl+"/favorites", //Favorites endpoint
-        headers: {
-          'OMDB_API_KEY': 'demo'
-        }
     }).then((response) => {
         expect(response.status).to.eq(200); //Expecting status code 200 for successful retrieval.
     });
@@ -47,9 +41,6 @@ describe("Endpoint Test Suite", () => {
     cy.request({
         method: 'GET', //Method GET to search for movies
         url: backendUrl+"/movies/search?apikey=demo&s="+testData.Title+"&page=1",  //Search endpoint with query parameters
-        headers: {  //Headers with API key
-          'OMDB_API_KEY': 'demo'
-        },
         failOnStatusCode: false //Bug: This test case returns an error code 500. 
     }).then((response) => {
         expect(response.status).to.eq(200); //Expecting status code 200 for successful search. But the actual response is 500.
@@ -60,9 +51,6 @@ describe("Endpoint Test Suite", () => {
     cy.request({
         method: 'DELETE',  //Delete method
         url: backendUrl+"/favorites/"+testData.imdbID, //Favorites endpoint with specific imdbID of the movie to be deleted is the same as the one added in the POST test case
-        headers: {
-          'OMDB_API_KEY': 'demo'
-        }
     }).then((response) => {
         expect(response.status).to.eq(200); //Expecting status code 200 for successful deletion.
     });
