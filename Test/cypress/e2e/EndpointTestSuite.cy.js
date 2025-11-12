897c4e06cca0f223d8a57fba86764959fa76a52f
Frankmer <frankmer@outlook.com>
2025-11-12 13:42:04 -0400

    Implement fixture in the testing files

--- Test/cypress/e2e/EndpointTestSuite.cy.js
+++ Test/cypress/e2e/EndpointTestSuite.cy.js
@@ -1,7 +1,10 @@
-var backendUrl = 'http://localhost:3000';
-describe("Endpoint Test Suite", () => {
+import requestData from '../fixtures/requestData.json';
 
-    it("GET / - health status", () => {
+var backendUrl = 'http://localhost:3000';
+
+describe("Health Check Endpoint", () => {
+
+  it("GET / - health status", () => {
     cy.request({
         method: 'GET',  //Method GET to check health status
         url: backendUrl+"/health" //Health endpoint
@@ -9,6 +12,11 @@ describe("Endpoint Test Suite", () => {
         expect(response.status).to.eq(200); //Expecting status code 200
     });
   });
+  
+});
+
+describe("Endpoint Test Suite", () => {
+  var testData = requestData.RequestData; //Importing test data from fixture
 
   it("POST /favorites - success", () => {
     cy.request({
@@ -17,16 +25,12 @@ describe("Endpoint Test Suite", () => {
         headers: {
           'OMDB_API_KEY': 'demo'    //Header with API key
         },
-        body: { //Request body with movie details
-            "imdbID": "tt1375667",
-            "Title": "Inception",
-            "Year": "2010",
-            "Poster": "testimage.jpg"
-        }
+        body: testData //Request body with movie details from fixture
     }).then((response) => {
         expect(response.status).to.eq(200); //Expecting status code 200 for successful addition.
     });
   });
+
   it("GET /favorites - success", () => {
     cy.request({
         method: 'GET', //Method GET to retrieve favorite movies
@@ -38,10 +42,11 @@ describe("Endpoint Test Suite", () => {
         expect(response.status).to.eq(200); //Expecting status code 200 for successful retrieval.
     });
   });
+
   it("GET /Search", () => {
     cy.request({
         method: 'GET', //Method GET to search for movies
-        url: backendUrl+"/movies/search?q=Inception&page=1",  //Search endpoint with query parameters
+        url: backendUrl+"/movies/search?apikey=demo&s="+testData.Title+"&page=1",  //Search endpoint with query parameters
         headers: {  //Headers with API key
           'OMDB_API_KEY': 'demo'
         },
@@ -50,10 +55,11 @@ describe("Endpoint Test Suite", () => {
         expect(response.status).to.eq(200); //Expecting status code 200 for successful search. But the actual response is 500.
     });
   });
+
   it("DELETE /favorites/:imdbID - success", () => { //Method DELETE to remove a movie from favorites
     cy.request({
         method: 'DELETE',  //Delete method
-        url: backendUrl+"/favorites/tt1375667", //Favorites endpoint with specific imdbID of the movie to be deleted is the same as the one added in the POST test case
+        url: backendUrl+"/favorites/"+testData.imdbID, //Favorites endpoint with specific imdbID of the movie to be deleted is the same as the one added in the POST test case
         headers: {
           'OMDB_API_KEY': 'demo'
         }
--- Test/cypress/e2e/QAPractice.cy.js
+++ Test/cypress/e2e/QAPractice.cy.js
@@ -1,7 +1,9 @@
-var movieTitle = "Inception d";
+import requestData from '../fixtures/requestData.json';
+
 var backendUrl = 'http://localhost:3000';
 
 describe('QA Practice', () => {
+  // Define the movie title to be used in this tests
   before(() => {
     // Runs once before all tests in the block
     cy.request({
@@ -10,26 +12,24 @@ describe('QA Practice', () => {
         headers: {
           'OMDB_API_KEY': 'demo'    //Header with API key
         },
-        body: { //Request body with movie details
-            "imdbID": "tt1375667",
-            "Title": movieTitle,
-            "Year": "2010",
-            "Poster": "testimage.jpg"
-        }
+        body: requestData.RequestData //Request body with movie details from fixture
     })
   })
-  
+  beforeEach(() => {
+    // Runs before each test in the block
+    cy.viewport(1280, 720); //Set viewport size for consistency
+    cy.visit('/') //Visit the base URL before each test
+  });
+
   it('Search Movie', () => {
-    cy.visit('/') //Visit the base URL
-    cy.get('input[placeholder="Search movies..."]').type(movieTitle) //Type movie title in search input
+    cy.get('input[placeholder="Search movies..."]').type(requestData.RequestData.Title) //Type movie title in search input
     cy.get('button[type="submit"]').click() //Click the search button
-    cy.get("h4").contains(movieTitle).should("be.visible") //Verify that the movie appears in the results
+    cy.get("h4").contains(requestData.RequestData.Title).should("be.visible") //Verify that the movie appears in the results
   })
 
   it('Remove from Favorites', () => {
-    cy.visit('/')
     //I did create a test movie with this title to test the remove from favorites functionality
-    cy.get("h4").contains(movieTitle).parent().find("button").click() 
-    cy.get("h4").contains(movieTitle).should("not.exist") //Verify that the movie has been removed from favorites
+    cy.get("h4").contains(requestData.RequestData.Title).parent().find("button").click() 
+    cy.get("h4").contains(requestData.RequestData.Title).should("not.exist") //Verify that the movie has been removed from favorites
   })  
 })
