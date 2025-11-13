e5fefda77bdcd1d229214c32fa7f962c33aaf7b4
Frankmer <frankmer@outlook.com>
2025-11-12 20:10:56 -0400

    Implement reporting tool to the testing

--- Test/cypress.config.js
+++ Test/cypress.config.js
@@ -1,8 +1,22 @@
 module.exports = {
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true, // Enable charts in the report
+    reportPageTitle: 'Cypress Test Report', // Custom title for the report
+    embeddedScreenshots: true, // Embed screenshots for failed tests
+    inlineAssets: true, // Embed CSS and JS directly into the HTML
+    saveAllAttempts: false, // Save reports for all test attempts (if retries are configured)
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    baseUrl: 'http://localhost:3001'
+    baseUrl: 'http://localhost:3001',
+    trashAssetsBeforeRuns: true,
+    video: true,
+    screenshotOnRunFailure: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
   },
 };
